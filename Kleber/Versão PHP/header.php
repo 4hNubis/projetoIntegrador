38723e0ea14af3e79668c8fe2c59254f962672da
4hNubis <kleberifes1925@gmail.com>
2022-10-05 00:09:08 -0300

    Re Organização do Projeto
    Mudanças: 
    Mudança na estruturação dos arquivos.
    Mudança na estrutura das paginas.
    Mudança no contêiner e nos construtores para melhor manutenção.

---
+++ Kleber/Versão PHP/header.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="cabecalho">
+        <div class="logo"><a href="home.php">
+            <p>Deka´s Confeitaria</p>    
+        </a></div>
+        <input type="text" id="pesquisa" placeholder="Pesquise ...">
+        <nav class="menu">
+            <li><a href="home.php">Home</a></li>
+            <li><a href="">Contato</a></li> 
+            <li ><a href="login.php">Login</a></li>   
+        </nav>
+    </div>
+</header>
